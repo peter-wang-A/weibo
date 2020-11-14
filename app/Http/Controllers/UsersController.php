32905de0fc216649bfa9c0609d9f04009049e1bb
vagrant <vagrant@homestead>
2020-11-14 06:58:32 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
+    //用户注册
     public function signup(){
         return view('users.create');
     }
+    //显示用户页面
+    public function show(User $user){
+        return view('users.show',compact('user'));
+    }
+
 }
