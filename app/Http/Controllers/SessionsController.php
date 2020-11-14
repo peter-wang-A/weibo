e04b146e45774018a1e5ff02949fab9b02ba9ba5
wyl <444079353@qq.com>
2020-11-14 22:34:36 +0800

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,27 +7,36 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest',[
+            'only'=>['create']
+        ]);
+    }
     //登录页面
     public function create()
     {
         return view('sessions.create');
     }
-    //非空验证密码
+    //登录
     public function store(Request $request)
     {
+        //非空验证密码
         $credentails = $this->validate($request, [
             'email' => 'required|email|max:255',
             'password' => 'required',
         ]);
 
-        if (Auth::attempt($credentails,$request->has('remenber'))) {
+        if (Auth::attempt($credentails, $request->has('remenber'))) {
             session()->flash('success', '欢迎回来');
-            return redirect()->route('users.show', Auth::user());
-        }else{
-         session()->flash('danger','邮箱或密码不正确');
-         return redirect()->back()->withInput();
+            $fallback = route('users.show', Auth::user());
+            return redirect()->intended($fallback);
+        } else {
+            session()->flash('danger', '邮箱或密码不正确');
+            return redirect()->back()->withInput();
         }
     }
+    //退出
     public function destroy()
     {
         Auth::logout();
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,20 @@ use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        //中间件过滤，except 方法过滤不需要登录页面，其他马面登录后菜课访问
+        // $this->middleware('auth',[
+        //     'except'=>['show','create','store']
+        // ]);
+        $this->middleware('auth', [
+            'except' => ['create', 'store']
+        ]);
+        //只让未登录用户访问注册页面
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
     //用户注册
     public function signup()
     {
@@ -39,24 +53,27 @@ class UsersController extends Controller
     //编辑资料页面
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
     //更新数据
     public function update(User $user, Request $request)
     {
+        $this->authorize('update', $user);
         //验证输入信息
         $this->validate($request, [
             'name' => 'required|unique:users|max:50',
             'password' => 'nullable|confirmed|min:6'
         ]);
-        $data=[];
-        $data['name']=$request->name;
-        if($request->password){
-            $data['password']=bcrypt($request->password);
+        $data = [];
+        $data['name'] = $request->name;
+        //如果又密码则更改没有则不更改
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
         }
         //插入数据库
         $user->update($data);
-        session()->flash('success','个人资料修改成功');
+        session()->flash('success', '个人资料修改成功');
         return redirect()->route('users.show', $user->id);
     }
 }
