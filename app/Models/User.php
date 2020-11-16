55043462875fb905bb41c21f14c290db840203b4
vagrant <vagrant@homestead>
2020-11-16 05:52:51 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -68,4 +68,43 @@ class User extends Authenticatable
     {
         return $this->statuses()->orderBy('created_at', 'desc');
     }
+
+
+    //获取粉丝列表
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    //获取关注的人列表
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    //关注
+    public function follow($user_ids)
+    {
+        //如果不是数组转成数组，sync接收数组
+        if (!is_array($user_ids)) {
+            $user_ids = compact("user_ids");
+        }
+        //再关注人列表里添加关注人，存在则不添加
+        $this->followings()->sync('user_ids', false);
+    }
+
+    //取消
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach('user_ids');
+    }
+
+    //判断当前登录的用户 A 是否关注了 B
+    public function is_followings($user_ids)
+    {
+        $this->followings()->contains($user_ids);
+    }
 }
---
+++ database/migrations/2020_11_16_051536_create_followers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFollowersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('followers', function (Blueprint $table) {
+            $table->increments('id');
+            //关注人列表
+            $table->integer('user_id')->index();
+            //粉丝列表
+            $table->integer('follower_id')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('followers');
+    }
+}
